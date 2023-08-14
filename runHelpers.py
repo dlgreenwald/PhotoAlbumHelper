615e6ad9253dc154d38871cab0d580a432e4fb3e
dlgreenwald <dlgreenwald@users.noreply.github.com>
2023-08-14 10:51:51 -0400

    Change to different schedular library that actually works

--- runHelpers.py
+++ runHelpers.py
@@ -1,20 +1,66 @@
 import datetime as dt
 
-from scheduler import Scheduler
-from scheduler.trigger import Saturday, Sunday
+import schedule
 import time
 import timelineEvents
 import ManageTimeAlbums
 import heartbeat
 
+schedule.every(30).minutes.do(heartbeat.beat)
+schedule.every().saturday.do(timelineEvents.detectAndCreateEvents)
+schedule.every().sunday.do(ManageTimeAlbums.createInPastAlbums)
 
-schedule = Scheduler()
-schedule.cyclic(dt.timedelta(minutes=30), heartbeat.beat)
-schedule.weekly(Saturday, timelineEvents.detectAndCreateEvents)
-schedule.weekly(Sunday, ManageTimeAlbums.createInPastAlbums)
+def format(job):
+        def format_time(t):
+            return t.strftime("%Y-%m-%d %H:%M:%S") if t else "[never]"
 
-print(schedule)
+        def is_repr(j):
+            return not isinstance(j, Job)
+
+        timestats = "(last run: %s, next run: %s)" % (
+            format_time(job.last_run),
+            format_time(job.next_run),
+        )
+
+        if hasattr(job.job_func, "__name__"):
+            job_func_name = job.job_func.__name__
+        else:
+            job_func_name = repr(job.job_func)
+
+        if job.job_func is not None:
+            args = [repr(x) if is_repr(x) else str(x) for x in job.job_func.args]
+            kwargs = ["%s=%s" % (k, repr(v)) for k, v in job.job_func.keywords.items()]
+            call_repr = job_func_name + "(" + ", ".join(args + kwargs) + ")"
+        else:
+            call_repr = "[None]"
+
+        if job.at_time is not None:
+            return "Every %s %s at %s do %s %s" % (
+                job.interval,
+                job.unit[:-1] if job.interval == 1 else job.unit,
+                job.at_time,
+                call_repr,
+                timestats,
+            )
+        else:
+            fmt = (
+                "Every %(interval)s "
+                + ("to %(latest)s " if job.latest is not None else "")
+                + "%(unit)s do %(call_repr)s %(timestats)s"
+            )
+
+            return fmt % dict(
+                interval=job.interval,
+                latest=job.latest,
+                unit=(job.unit[:-1] if job.interval == 1 else job.unit),
+                call_repr=call_repr,
+                timestats=timestats,
+            )
+
+all_jobs = schedule.get_jobs()
+for job in all_jobs:
+    print(format(job))
 
 while True:
-    schedule.exec_jobs()
+    schedule.run_pending()
     time.sleep(1)
