d0c77a2386d5e3a155c0f83f926374c50be91dac
dlgreenwald <dlgreenwald@users.noreply.github.com>
2024-12-11 21:13:33 -0500

    generate swagger-client for photoprism

---
+++ swagger_client/api/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.albums_api import AlbumsApi
+from swagger_client.api.authentication_api import AuthenticationApi
+from swagger_client.api.config_api import ConfigApi
+from swagger_client.api.download_api import DownloadApi
+from swagger_client.api.errors_api import ErrorsApi
+from swagger_client.api.faces_api import FacesApi
+from swagger_client.api.files_api import FilesApi
+from swagger_client.api.folders_api import FoldersApi
+from swagger_client.api.images_api import ImagesApi
+from swagger_client.api.labels_api import LabelsApi
+from swagger_client.api.library_api import LibraryApi
+from swagger_client.api.links_api import LinksApi
+from swagger_client.api.markers_api import MarkersApi
+from swagger_client.api.photos_api import PhotosApi
+from swagger_client.api.server_api import ServerApi
+from swagger_client.api.services_api import ServicesApi
+from swagger_client.api.settings_api import SettingsApi
+from swagger_client.api.stacks_api import StacksApi
+from swagger_client.api.subjects_api import SubjectsApi
+from swagger_client.api.users_api import UsersApi
+from swagger_client.api.videos_api import VideosApi
