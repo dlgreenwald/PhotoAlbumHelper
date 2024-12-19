fbcb58f84e008e7e9a4d2859c014a7c84ef5a721
dlgreenwald <dlgreenwald@users.noreply.github.com>
2024-12-18 19:25:18 -0500

    Change schedular output format to better indicate progress and time till next run

--- runHelpers.py
+++ runHelpers.py
@@ -1,13 +1,12 @@
-import datetime as dt
+import time
+import threading
 
 import schedule
-import time
+from dotenv import load_dotenv
+
 import timelineEvents
 import ManageTimeAlbums
 import emailAlerts
-import heartbeat
-import sys
-import threading
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
@@ -16,64 +15,83 @@ def run_threaded(job_func):
 
 # I didn't like the format of the print output of the schedule so the format has been lifted and reused.
 def format(job):
-        def format_time(t):
-            return t.strftime("%Y-%m-%d %H:%M:%S") if t else "[never]"
+    def format_time(t):
+        return t.strftime("%Y-%m-%d %H:%M:%S") if t else "[never]"
 
-        def is_repr(j):
-            return not isinstance(j, schedule.Job)
+    def is_repr(j):
+        return not isinstance(j, schedule.Job)
 
-        timestats = "(last run: %s, next run: %s)" % (
-            format_time(job.last_run),
-            format_time(job.next_run),
+    timestats = "(last run: %s, next run: %s)" % (
+        format_time(job.last_run),
+        format_time(job.next_run),
+    )
+
+    if hasattr(job.job_func, "__name__"):
+        job_func_name = job.job_func.__name__
+    else:
+        job_func_name = repr(job.job_func)
+
+    if job.job_func is not None:
+        args = [repr(x) if is_repr(x) else str(x) for x in job.job_func.args]
+        kwargs = ["%s=%s" % (k, repr(v)) for k, v in job.job_func.keywords.items()]
+        call_repr = job_func_name + "(" + ", ".join(args + kwargs) + ")"
+    else:
+        call_repr = "[None]"
+
+    if job.at_time is not None:
+        return "Every %s %s at %s do %s %s" % (
+            job.interval,
+            job.unit[:-1] if job.interval == 1 else job.unit,
+            job.at_time,
+            call_repr,
+            timestats,
+        )
+    else:
+        fmt = (
+            "Every %(interval)s "
+            + ("to %(latest)s " if job.latest is not None else "")
+            + "%(unit)s do %(call_repr)s %(timestats)s"
         )
 
-        if hasattr(job.job_func, "__name__"):
-            job_func_name = job.job_func.__name__
+        return fmt % dict(
+            interval=job.interval,
+            latest=job.latest,
+            unit=(job.unit[:-1] if job.interval == 1 else job.unit),
+            call_repr=call_repr,
+            timestats=timestats,
+        )
+
+def schedule_and_run():
+    print("Scheduling Jobs:")
+    #    schedule.every(30).minutes.do(run_threaded, heartbeat.beat)
+    schedule.every(1).day.at("01:00").do(run_threaded, timelineEvents.detectAndCreateEvents)
+    schedule.every().sunday.do(run_threaded, ManageTimeAlbums.create_in_past_albums)
+    schedule.every().sunday.do(run_threaded, emailAlerts.emailAlert)
+
+    last_wait = -1
+
+    sleep_len = 1
+    while True:
+        next_job = schedule.idle_seconds()
+        if last_wait > next_job:
+            # We are continuing to progress towards the next job
+            last_wait = next_job
+            print("*", flush=True, end="")
         else:
-            job_func_name = repr(job.job_func)
+            # We will start a new job
+            print("\n")
+            schedule.run_pending()
 
-        if job.job_func is not None:
-            args = [repr(x) if is_repr(x) else str(x) for x in job.job_func.args]
-            kwargs = ["%s=%s" % (k, repr(v)) for k, v in job.job_func.keywords.items()]
-            call_repr = job_func_name + "(" + ", ".join(args + kwargs) + ")"
-        else:
-            call_repr = "[None]"
-
-        if job.at_time is not None:
-            return "Every %s %s at %s do %s %s" % (
-                job.interval,
-                job.unit[:-1] if job.interval == 1 else job.unit,
-                job.at_time,
-                call_repr,
-                timestats,
-            )
-        else:
-            fmt = (
-                "Every %(interval)s "
-                + ("to %(latest)s " if job.latest is not None else "")
-                + "%(unit)s do %(call_repr)s %(timestats)s"
-            )
-
-            return fmt % dict(
-                interval=job.interval,
-                latest=job.latest,
-                unit=(job.unit[:-1] if job.interval == 1 else job.unit),
-                call_repr=call_repr,
-                timestats=timestats,
-            )
+            sleep_len =  next_job/100
+            last_wait = next_job
+            all_jobs = schedule.get_jobs()
+            for job in all_jobs:
+                print(format(job))
+            print("0                       25                        50                       75                     100")
+            print("-----------------------------------------------------------------------------------------------------")
+        time.sleep(sleep_len)
 
 
-print("Schedualing Jobs:")
-schedule.every(30).minutes.do(run_threaded, heartbeat.beat)
-schedule.every(1).day.at("01:00").do(run_threaded, timelineEvents.detectAndCreateEvents)
-schedule.every().sunday.do(run_threaded, ManageTimeAlbums.createInPastAlbums)
-schedule.every().sunday.do(run_threaded, emailAlerts.emailAlert)
-all_jobs = schedule.get_jobs()
-for job in all_jobs:
-    print(format(job))
-
-while True:
-    schedule.run_pending()
-    #explicit flush to get logging in docker to work
-    sys.stdout.flush()
-    time.sleep(1)
+if __name__=="__main__":
+    load_dotenv()
+    schedule_and_run()
